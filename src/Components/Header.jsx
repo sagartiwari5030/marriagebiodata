cf7803a181da63b0326af4a4b6674f0c570a4cba
Sagar Tiwari <sagartiwari5030@gmail.com>
2024-09-27 20:05:50 +0530

    making navbar responsive
    making navbar responsive in mobile view

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,3 @@
-// src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -7,12 +6,14 @@ function Header() {
     <header className="bg-white text-white p-4 sticky top-0 z-50 shadow-md">
       <nav className="container mx-auto flex justify-between items-center">
         {/* Left Section - Logo */}
-        <Link to="/" className="text-2xl text-pink-600 font-bold">MarriageBiodata</Link>
+        <Link to="/" className="text-xl md:text-2xl text-pink-600 font-bold">
+          MarriageBiodata
+        </Link>
 
         {/* Right Section - Button */}
         <div className="space-x-4">
           <Link to="/create-profile">
-            <button className="bg-blue-700 text-white px-6 py-2 rounded-full hover:bg-blue-800 transition duration-300">
+            <button className="bg-blue-700 text-sm md:text-base text-white px-4 md:px-6 py-2 rounded-full hover:bg-blue-800 transition duration-300">
               Create Biodata Now!
             </button>
           </Link>
